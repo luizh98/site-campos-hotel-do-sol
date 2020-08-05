0161c9c0e219860d0026e7c04addd97ebfef5edc
luizh98 <dev.lhsantos@gmail.com>
2020-08-04 21:11:25 -0300

    Adicionamos os textos

--- contact_process.php
+++ contact_process.php
@@ -1,11 +1,9 @@
 <?php
-
     $to = "dev.lhdsantos@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+	$message = $_REQUEST['message'];
 
     $headers = "From: $from";
 	$headers = "From: " . $from . "\r\n";
@@ -13,7 +11,7 @@
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Cake Template.";
+	$subject = "You have a message from your Cake Template.";
 
     $logo = '';
     $link = '#';
@@ -26,9 +24,9 @@
 	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
 	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
 	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
 	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+	$body .= "<tr><td colspan='2' style='border:none;'>{$message}</td></tr>";
 	$body .= "</tbody></table>";
 	$body .= "</body></html>";
 
